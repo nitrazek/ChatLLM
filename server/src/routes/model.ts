abbaa5aea6c433dfe0908fe9c07fb4c86f0600bd
xShyQx <93669491+xShyQx@users.noreply.github.com>
2024-07-08 19:42:05 +0200

    Started to receiving answer as stream and trying to send it back from server

--- server/src/routes/model.ts
+++ server/src/routes/model.ts
@@ -8,6 +8,8 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 import { BaseMessageChunk } from "langchain/schema";
 import { getChromaConnection } from "../services/chroma";
 import { Chroma } from "@langchain/community/vectorstores/chroma";
+import http from "http";
+import { } from "stream";
 
 const template = `
 You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question.
@@ -17,12 +19,12 @@ Context: {context}
 Answer:`;
 
 const questionsRoute = async (fastify: FastifyInstance) => {
-  fastify.post<{ Body: QuestionType, Reply: AnswerType }>("/questions", {
+  fastify.post<{ Body: QuestionType/*, Reply: AnswerType*/ }>("/questions", {
     schema: {
       body: Question,
-      response: {
-        200: Answer
-      }
+      // response: {
+      //   200: Answer
+      // }
     }
   }, async (request, response) => {
     const chroma: Chroma = await getChromaConnection();
@@ -41,8 +43,27 @@ const questionsRoute = async (fastify: FastifyInstance) => {
     ]);
 
     const question: string = request.body.question;
+
+    // const options = { hostname: "ollama", port: 11434, path: "/api/generate", method: "POST" }
+    // const ollamaReq = http.request(options, (ollamaRes) => {
+    //   ollamaRes.on("data", (data) => {
+    //     console.log(data);
+    //     console.log("--------------------");
+    //   })
+    //   ollamaRes.on("end", () => {
+    //     console.log("koniec");
+    //   })
+    // })
+    // ollamaReq.write(JSON.stringify({ model: "llama3", prompt: "Why is the sky blue?" }));
+    // ollamaReq.end();
+
     const messageChunk: BaseMessageChunk = await ollama.invoke(question);
-    response.status(200).send({ answer: messageChunk.content as string })
+    const message: string = messageChunk.content as string;
+    // response.status(200).send({ answer: message })
+    const stream = await ollama.stream(question);
+    response.header("Content-Type", "application/x-ndjson");
+    response.status(200).send(stream);
+    return response;
   });
 };
 
