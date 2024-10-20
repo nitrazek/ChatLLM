e243687f429d26324906e14d622ca0b83938b4da
nitrazek <99548252+nitrazek@users.noreply.github.com>
2024-10-20 18:49:58 +0200

    Fixed user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -13,7 +13,7 @@ export class User extends ExtendedBaseEntity {
     id!: number;
 
     @Column()
-    @Length(4   , 30, { message: getIsInvalidMessage('Name') })
+    @Length(4, 30, { message: getIsInvalidMessage('Name') })
     name!: string;
 
     @Column()
@@ -30,6 +30,7 @@ export class User extends ExtendedBaseEntity {
 
     @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
     @IsEnum(UserRole, { message: getIsInvalidMessage('Role') })
+    @IsOptional()
     role!: UserRole;
 
     @OneToMany(() => Chat, (chat) => chat.user)
