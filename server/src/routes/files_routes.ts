a1aab0b16cf7f3c736716673fecac354dfff73fd
sh4dqwx <bart.karolczak@gmail.com>
2024-10-25 22:34:26 +0200

    Added endpoints for saving file to specific folder, getting list of files from folder, updating file metadata, deleting file

--- server/src/routes/files_routes.ts
+++ server/src/routes/files_routes.ts
@@ -6,16 +6,22 @@ import { resolveFileMimetype } from "../utils/files_handlers";
 import { ChromaService } from "../services/chroma_service";
 import { AuthHeader } from "../schemas/base_schemas";
 import { File } from "../models/file";
+import { getPaginationMetadata } from "../utils/pagination_handler";
+import path from "path";
+import { FileType } from "../enums/file_type";
+import { Auth, IsNull } from "typeorm";
 
 const filesRoutes: FastifyPluginCallback = (server, _, done) => {
     // Upload a file to the knowledge base (only admin)
     server.post<{
         Headers: AuthHeader,
+        Querystring: Schemas.UploadFileQuery,
         Reply: Schemas.UploadFileResponse
     }>('/upload', {
         schema: Schemas.UploadFileSchema,
         onRequest: [adminAuth(server)]
     }, async (req, reply) => {
+        const { folderId } = req.query;
         const multipartFile = await req.file();
         if (!multipartFile) throw new BadRequestError('No file was provided.');
 
@@ -23,12 +29,19 @@ const filesRoutes: FastifyPluginCallback = (server, _, done) => {
         if (!resolvedFile) throw new BadRequestError('This file type is not supported.');
         const [fileHandler, fileType] = resolvedFile;
 
+        const folder = folderId ? await File.findOneBy({ id: folderId }) : null;
+        if(folder && folder.type !== FileType.FOLDER)
+            throw new BadRequestError("Invalid folder id");
+
+        const baseFilename = path.basename(multipartFile.filename, path.extname(multipartFile.filename));
         let file = File.create({
-            name: multipartFile.filename,
+            name: baseFilename,
             type: fileType,
+            parent: folder,
             creator: req.user
         });
         file = await file.save();
+        console.dir(file);
 
         const chroma = await ChromaService.getInstance();
         await chroma.addDocuments([{
@@ -52,7 +65,74 @@ const filesRoutes: FastifyPluginCallback = (server, _, done) => {
         schema: Schemas.GetFileListSchema,
         onRequest: [adminAuth(server)]
     }, async (req, reply) => {
-        // dupa dupa twoja stara
+        const { page = 1, limit = 30, folderId } = req.query;
+        if(page < 1) throw new BadRequestError("Invalid page number, must not be negative");
+        if(limit < 1) throw new BadRequestError("Invalid limit value, must not be negative");
+
+        const folder = folderId ? await File.findOneBy({ id: folderId }) : null;
+        if(folder && folder.type !== FileType.FOLDER)
+            throw new BadRequestError("Invalid folder ID");
+
+        const [files, totalFiles] = await File.findAndCount({
+            skip: (page - 1) * limit,
+            take: limit,
+            where: { parent: folder !== null ? folder : IsNull() },
+            order: { name: "ASC" },
+            relations: ["creator"]
+        });
+
+        const paginationMetadata = getPaginationMetadata(page, limit, totalFiles);
+        if(paginationMetadata.currentPage > paginationMetadata.totalPages)
+            throw new BadRequestError("Invalid page number, must not be greater than page amount");
+
+        reply.send({
+            files: files.map(file => ({ ...file, creatorName: file.creator.name })),
+            pagination: paginationMetadata
+        });
+    });
+
+    //Change details of specified file (only admin)
+    server.put<{
+        Headers: AuthHeader,
+        Params: Schemas.UpdateFileParams,
+        Body: Schemas.UpdateFileBody,
+        Reply: Schemas.UpdateFileResponse
+    }>('/:fileId', {
+        schema: Schemas.UpdateFileSchema,
+        onRequest:[adminAuth(server)]
+    }, async(req, reply) => {
+        const file = await File.findOne({
+            where: { id: req.params.fileId },
+            relations: ["creator"]
+        });
+        if (!file) throw new BadRequestError('File do not exist.');
+
+        const { name } = req.body;
+        File.merge(file, { name });
+        const updatedFile = await file.save();
+
+        reply.send({ ...updatedFile, creatorName: updatedFile.creator.name });
+    })
+
+    // Delete specific file
+    server.delete<{
+        Headers: AuthHeader,
+        Params: Schemas.DeleteFileParams,
+        Reply: Schemas.DeleteFileResponse
+    }>('/:fileId', {
+        schema: Schemas.DeleteFileSchema,
+        onRequest:[adminAuth(server)]
+    }, async (req, reply) => {
+        const file = await File.findOneBy({ id: req.params.fileId });
+        if (!file) throw new BadRequestError('File do not exist.');
+    
+        const chroma = await ChromaService.getInstance();
+        await chroma.delete({
+            ids: [file.id.toString()]
+        })
+
+        await file.remove();
+        reply.code(204).send();
     });
 
     done();
--- server/src/schemas/files_schemas.ts
+++ server/src/schemas/files_schemas.ts
@@ -2,6 +2,7 @@ import { Static, Type } from "@sinclair/typebox"
 import { FastifySchema } from "fastify"
 import { AdminGuardedResponseSchema, UserGuardedResponseSchema } from "./errors_schemas";
 import { AuthHeaderTypes, PaginationMetadataTypes } from "./base_schemas";
+import { FileType } from "../enums/file_type";
 
 //////////////////// Generic Schemas ////////////////////
 
@@ -9,6 +10,8 @@ import { AuthHeaderTypes, PaginationMetadataTypes } from "./base_schemas";
 const GenericFileResponseTypes = Type.Object({
     id: Type.Number({ description: "Unique identifier of the file" }),
     name: Type.String({ description: "Name of the file" }),
+    type: Type.Enum(FileType, { description: "Type of the file" }),
+    creatorName: Type.String({ description: "Name of the user who created that file" }),
     createdAt: Type.Unsafe<Date>({ type: 'string', format: 'date', description: "Creation date of the file" }),
     updatedAt: Type.Unsafe<Date>({ type: 'string', format: 'date', description: "Update date of the file" })
 }, { description: "Basic information about a file" });
@@ -16,15 +19,20 @@ const GenericFileResponseTypes = Type.Object({
 //////////////////// Schemas for POST requests ////////////////////
 
 // Schema for uploading file to files knowledge base
-const UploadFileResponseTypes = Type.Object({
-}, { description: "Empty response for a successful file upload" });
+const UploadFileQueryTypes = Type.Object({
+    folderId: Type.Optional(Type.Number({ description: "Folder in which uploaded file will be saved" }))
+});
+export type UploadFileQuery = Static<typeof UploadFileQueryTypes>;
+
+const UploadFileResponseTypes = Type.Object({}, { description: "Empty response for a successful file upload" });
 export type UploadFileResponse = Static<typeof UploadFileResponseTypes>;
 
 export const UploadFileSchema: FastifySchema = {
     summary: "Upload a file",
-    description: "Endpoint to upload a file (txt or pdf) to the knowledge base. The file is processed and its content is extracted for storage in the knowledge base.",
+    description: "Endpoint to upload a file (txt or pdf) to the knowledge base. The file is processed and its content is extracted for storage in the knowledge base. Only accessible by admin users.",
     consumes: ['multipart/form-data'],
     headers: AuthHeaderTypes,
+    querystring: UploadFileQueryTypes,
     tags: ["Files"],
     response: {
         204: UploadFileResponseTypes,
@@ -38,7 +46,8 @@ export const UploadFileSchema: FastifySchema = {
 // Schema for getting list of files from knowledge base
 const GetFileListQueryTypes = Type.Object({
     page: Type.Optional(Type.Number({ description: "The page number for pagination" })),
-    limit: Type.Optional(Type.Number({ description: "The number of files to retrieve per page" }))
+    limit: Type.Optional(Type.Number({ description: "The number of files to retrieve per page" })),
+    folderId: Type.Optional(Type.Number({ description: "Folder from which files will be received" }))
 }, { description: "Query parameters for paginated chat list retrieval" });
 export type GetFileListQuery = Static<typeof GetFileListQueryTypes>;
 
@@ -52,7 +61,7 @@ export type GetFileListResponse = Static<typeof GetFileListResponseTypes>;
 
 export const GetFileListSchema: FastifySchema = {
     summary: "Get list of files",
-    description: "Retrieves a paginated list of files from knowledge base.",
+    description: "Retrieves a paginated list of files from knowledge base. Only accessible by admin users.",
     headers: AuthHeaderTypes,
     querystring: GetFileListQueryTypes,
     tags: ["Files"],
@@ -64,10 +73,54 @@ export const GetFileListSchema: FastifySchema = {
 
 //////////////////// Schemas for PUT requests ////////////////////
 
-// Empty for now
+// Schema for changing file details
+const UpdateFileParamsTypes = Type.Object({
+    fileId: Type.Number({ description: "ID of the file to update" })
+}, { description: "Parameters to identify the file to update" });
+export type UpdateFileParams = Static<typeof UpdateFileParamsTypes>;
 
+const UpdateFileBodyTypes = Type.Object({
+    name: Type.String({ description: "The new name for the file" })
+}, { description: "Payload containing the updated file details" });
+export type UpdateFileBody = Static<typeof UpdateFileBodyTypes>;
 
+const UpdateFileResponseTypes = Type.Object({
+    ...GenericFileResponseTypes.properties
+}, { description: "Response containing the updated file details" });
+export type UpdateFileResponse = Static<typeof UpdateFileResponseTypes>;
+
+export const UpdateFileSchema: FastifySchema = {
+    summary: "Update file details",
+    description: "Updates the name or settings of a specific file. Only accessible by admin users.",
+    headers: AuthHeaderTypes,
+    params: UpdateFileParamsTypes,
+    body: UpdateFileBodyTypes,
+    tags: ["Files"],
+    response: {
+        200: UpdateFileResponseTypes,
+        ...AdminGuardedResponseSchema
+    }
+};
 
 //////////////////// Schemas for DELETE requests ////////////////////
 
-// Empty for now
+// Schema for deleting a file
+const DeleteFileParamsTypes = Type.Object({
+    fileId: Type.Number({ description: "ID of the file to delete" })
+}, { description: "Parameters to identify the file to delete" });
+export type DeleteFileParams = Static<typeof DeleteFileParamsTypes>;
+
+const DeleteFileResponseTypes = Type.Object({}, { description: "Empty response for a successful file deletion" });
+export type DeleteFileResponse = Static<typeof DeleteFileResponseTypes>;
+
+export const DeleteFileSchema: FastifySchema = {
+    summary: "Delete file",
+    description: "Deletes a specific file. Only accessible by admin users.",
+    headers: AuthHeaderTypes,
+    params: DeleteFileParamsTypes,
+    tags: ["Files"],
+    response: {
+        204: DeleteFileResponseTypes,
+        ...AdminGuardedResponseSchema
+    }
+}
