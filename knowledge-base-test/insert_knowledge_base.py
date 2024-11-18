146f18a67724338dc58cf506620f08bf6b98e179
sh4dqwx <bart.karolczak@gmail.com>
2024-11-18 16:05:25 +0100

    Added files about few products and finished script

--- knowledge-base-test/insert_knowledge_base.py
+++ knowledge-base-test/insert_knowledge_base.py
@@ -7,36 +7,58 @@ BASE_URL = "http://localhost:3000/api/v1/files"
 class File(NamedTuple):
   id: int
   name: str
+  path: str
 
-def insert_file(file_url: str, parent_folder_url: str | None):
-  pass
+def insert_file(file: File, parent_folder: File):
+  with open(file.path, "rb") as opened_file:
+    requests.post(
+      url=f"{BASE_URL}/upload{'?folderId=' + parent_folder.id if parent_folder.id is not None else ''}",
+      files=opened_file
+    )
 
-def insert_folder(folder_url: str, parent_folder_url: str | None):
-  folder_name = os.path.basename(folder_url)
-  parent_folder_name = os.path.basename(parent_folder_url) if parent_folder_url is not None else None
-
-  requests.post(
-    url=f"{BASE_URL}/folders/new",
-    json={
-      "name": folder_name,
-      ""
-    }
-  )
-
-  for file in os.listdir(folder):
-    abs_path = os.path.join(base_folder, file)
-    if os.path.isdir(abs_path):
-      print("folder")
-    print(file)
+def insert_folder(folder: File, parent_folder: File, only_content: bool = False):
+  if not only_content:
+    insertFolderResponse = requests.post(
+      url=f"{BASE_URL}/folders/new",
+      json={
+        "name": folder.name,
+        "parentFolderId": parent_folder.id
+      }
+    ).json
+    parent_folder = File(
+      id=insertFolderResponse.id,
+      name=insertFolderResponse.name,
+      path=parent_folder.path
+    )
+  
+  for file in os.listdir(folder.path):
+    file = File(
+      id=None,
+      name=file,
+      path=os.path.join(folder.path, file.name)
+    )
+    if os.path.isdir(file.path):
+      insert_folder(
+        folder=file,
+        parent_folder=folder
+      )
+    else:
+      insert_file(
+        file=file,
+        parent_folder=folder
+      )
 
 def main():
-  base_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "knowledge-base-files")
-  for file in os.listdir(base_folder):
-    abs_path = os.path.join(base_folder, file)
-    if os.path.isdir(abs_path):
-      insert_folder(folder_url=file, parent_folder_url=None)
-    else:
-      insert_file(file_url=file, parent_folder_url=None)
+  folder = File(
+    id=None,
+    name="knowledge-base-files",
+    path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "knowledge-base-files")
+  )
+  insert_folder(
+    folder=folder,
+    parent_folder=folder,
+    only_content=True
+  )
 
 if __name__ == "__main__":
   main()
